3f960b66f577029b2f0b70946f8fc3b9f67a49e2
Helen Marshall <marshallhelenm@gmail.com>
2019-07-10 15:25:32 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -33,9 +33,9 @@ end
 def hit?(card_total)
  prompt_user
  user_input = get_user_input
- if user_input = 'h'
+ if user_input == 'h'
   card_total += deal_card
-  elsif user_input = 's'
+  elsif user_input == 's'
     card_total = card_total
   else 
     invalid_command
