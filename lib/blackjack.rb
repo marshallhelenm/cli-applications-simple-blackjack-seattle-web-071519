c9b033f776e4c417f482f2e3735fdd5ca62ade07
Helen Marshall <marshallhelenm@gmail.com>
2019-07-10 15:06:12 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,5 +1,5 @@
 def welcome
-  # code #welcome here
+  puts "Welcome to the Blackjack Table"
 end
 
 def deal_card
@@ -8,7 +8,6 @@ end
 
 def display_card_total(card_total)
   puts "Your cards add up to #{card_total}"
-  # code #display_card_total here
 end
 
 def prompt_user
